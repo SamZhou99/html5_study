56b3d149d7cd07fbd8fa1d6106fe3c38cfb649d9
sam.z <45636987@qq.com>
2019-01-05 00:41:48 +0800

    init

---
+++ pixi/js/my.js
@@ -0,0 +1,23 @@
+var my = {
+	init(backgroundColor=0x000000, ){
+		var app = new PIXI.Application(1, 1, {
+			backgroundColor : backgroundColor,
+			antialias: true,
+			transparent: false
+		});
+		app.renderer.autoResize = true;
+		app.renderer.resize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(app.view);
+		return app;
+	},
+	load(assetUrlArr, completeEvent){
+		if (!assetUrlArr) { return; }
+		if (!completeEvent) { return; }
+
+		if (assetUrlArr.length <= 0) { completeEvent();return; }
+
+		PIXI.loader.add(assetUrlArr).load((loader, resources)=>{
+			completeEvent(loader, resources);
+		});
+	}
+};
